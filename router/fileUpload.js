44c9482cc951b7c6d7f8aacb3b200d5b1b26ec69
Hemanth <hemanth.sani@gmail.com>
2022-06-27 11:39:34 +0530

    access-control changes

--- router/fileUpload.js
+++ router/fileUpload.js
@@ -54,7 +54,10 @@ const router = express.Router();
 
 
 router.post("/upload",upload.array('upload'),(req,res,next)=>{
-    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
 //     setTimeout(()=>{
         
 //         console.log(candidateId);
