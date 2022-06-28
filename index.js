fd951c3cf9f64c8802ea5994f0f58234b4eac7db
Hemanth <hemanth.sani@gmail.com>
2022-06-28 17:26:46 +0530

    CORS changes

--- index.js
+++ index.js
@@ -43,7 +43,10 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
   // console.log(req.hostname, req.headers, req.path);
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
   try {
     
     const allowedMethods = ["POST", "GET", "PUT", "DELETE"];
