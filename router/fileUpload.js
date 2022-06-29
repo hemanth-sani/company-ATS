e5aa26bee84b28ad00a9ce0feb7d8e137eddce8b
Hemanth <hemanth.sani@gmail.com>
2022-06-29 18:36:41 +0530

    file changes

--- router/fileUpload.js
+++ router/fileUpload.js
@@ -1,48 +1,55 @@
-// const aws = require('aws-sdk');
-// const multer = require('multer');
-// const multerS3 = require('multer-s3');
-// const uuid = require('uuid').v4;
-// const Candidate= require('../models/candidate');
-// const response = require('../utils/Response');
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const uuid = require('uuid').v4;
 
-// const path = require('path');
-// const express = require('express');
+const path = require('path');
+const express = require('express');
+
+const s3 = new aws.S3({
+  accessKeyId:process.env.AWS_Hemanth_ID,
+  secretAccessKey:process.env.AWS_Hemanth_Key
+});
 
 
-// const s3= new aws.S3({
-//     accessKeyId:process.env.AWSHemanthID,
-//     secretAccessKey:process.env.AWSHemanthKey
+var unique;
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'hemanth-fileupload',
+        metadata: (req,file, cb)=>{
+            cb(null,{fieldName: file.fieldname});
+        },
+        key:(req, file, cb) =>{
+            const ext = file.originalname;
+            unique = uuid();
+            cb(null, `${unique}-${ext}`);
+            console.log(unique);
+        }
+    })
+});
+
+
+// const storage = multer.diskStorage({
+//     destination:(req,file,cb) =>{
+//         cb(null,'uploads');
+//     },
+//     filename:(req,file,cb)=>{
+//         const { originalname }=file;
+//         cb(null, `${uuid()}-${originalname}`);
+//     }
 // });
 
-
-// var unique;
-// const upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: 'hemanth-upload',
-//         metadata: (req,file, cb)=>{
-//             cb(null,{fieldName: file.fieldname});
-//         },
-//         key:(req, file, cb) =>{
-//             const ext = file.originalname;
-//             unique = uuid();
-//             cb(null, `${unique}-${ext}`);
-//             console.log(unique);
-//         },
-//     })
-// });
+// const upload = multer({storage});
 
 
 
-// const router = express.Router();
+const router = express.Router();
 
 
-// router.post("/uploads",upload.array('file'),(req,res,next)=>{
-//     console.log(req.body);
-// });
+router.post("/upload",upload.array('upload'));
 
-// module.exports = router;
-
+module.exports = router;
 
 
 
