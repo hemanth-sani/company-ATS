c0f69c7bef591b5dc165a8a6eb03ea0eb497532b
Hemanth <hemanth.sani@gmail.com>
2022-06-29 16:42:56 +0530

    file

--- router/fileUpload.js
+++ router/fileUpload.js
@@ -1,47 +1,47 @@
-// const aws = require('aws-sdk');
-// const multer = require('multer');
-// const multerS3 = require('multer-s3');
-// const uuid = require('uuid').v4;
-// const Candidate= require('../models/candidate');
-// const response = require('../utils/Response');
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const uuid = require('uuid').v4;
+const Candidate= require('../models/candidate');
+const response = require('../utils/Response');
 
-// const path = require('path');
-// const express = require('express');
-
-
-// const s3= new aws.S3({
-//     accessKeyId:process.env.AWSHemanthID,
-//     secretAccessKey:process.env.AWSHemanthKey
-// });
+const path = require('path');
+const express = require('express');
 
 
-// var unique;
-// const upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: 'hemanth-upload',
-//         metadata: (req,file, cb)=>{
-//             cb(null,{fieldName: file.fieldname});
-//         },
-//         key:(req, file, cb) =>{
-//             const ext = file.originalname;
-//             unique = uuid();
-//             cb(null, `${unique}-${ext}`);
-//             console.log(unique);
-//         },
-//     })
-// });
+const s3= new aws.S3({
+    accessKeyId:process.env.AWSHemanthID,
+    secretAccessKey:process.env.AWSHemanthKey
+});
+
+
+var unique;
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'hemanth-upload',
+        metadata: (req,file, cb)=>{
+            cb(null,{fieldName: file.fieldname});
+        },
+        key:(req, file, cb) =>{
+            const ext = file.originalname;
+            unique = uuid();
+            cb(null, `${unique}-${ext}`);
+            console.log(unique);
+        },
+    })
+});
 
 
 
-// const router = express.Router();
+const router = express.Router();
 
 
-// router.post("/uploads",upload.array('file'),(req,res,next)=>{
-//     console.log(req.body);
-// });
+router.post("/uploads",upload.array('file'),(req,res,next)=>{
+    console.log(req.body);
+});
 
-// module.exports = router;
+module.exports = router;
 
 
 
