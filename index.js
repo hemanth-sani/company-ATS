c9e71b27fad9c2cfb5e6322f11e3aba2681cd50a
Hemanth <hemanth.sani@gmail.com>
2022-06-29 16:29:38 +0530

    changes

--- index.js
+++ index.js
@@ -3,24 +3,12 @@ const dotenv = require('dotenv');
 const log = require('./utils/bunyanLogger');
 const connectDB = require('./config/db')
 const router = require('./router/router')
-//
-// const aws = require('aws-sdk');
-// const multerS3 = require('multer-s3');
-// const uuid = require('uuid').v4;
-// const multer = require("multer");
-
-//
-const uploadFile = require('./utils/awsupload');
 const cors = require('cors');
 const { errorResponse } = require('./utils/Response')
 const error = require('./middleware/error')
 dotenv.config({ path: './config.env' });
-
-// const s3 = new aws.S3({apiVersion: '2006-03-01'});
 const app = express();
 const port = process.env.PORT || 5000;
-
-
 connectDB();
 
 app.use(express.json());
@@ -38,17 +26,29 @@ const corsOptions = {
 //     cb(null, `${Date.now()}_${file.originalname}`);
 //   },
 // });
+// var unique;
+// const upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: 'hemanth-fileupload',
+//         metadata: (req,file, cb)=>{
+//             cb(null,{fieldName: file.fieldname});
+//         },
+//         key:(req, file, cb) =>{
+//             const ext = file.originalname;
+//             unique = uuid();
+//             cb(null, `${unique}-${ext}`);
+//             console.log(unique);
+//         }
+//     })
+// });
 //
 
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
   // console.log(req.hostname, req.headers, req.path);
-  res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
+
   try {
-    
     const allowedMethods = ["POST", "GET", "PUT", "DELETE"];
     if (!allowedMethods.includes(req.method)) {
       // errorResponse({ status: 400, result: `${req.method} method is not allowed`, res })
@@ -61,30 +61,13 @@ app.use((req, res, next) => {
   next();
 });
 
-//
+
 //var upload = multer({ dest: "uploads/" });
 
-// var upload1 = multer({ storage: storage });
-// var unique;
-// const upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: 'hemanth-upload',
-//         metadata: (req,file, cb)=>{
-//             cb(null,{fieldName: file.fieldname});
-//         },
-//         key:(req, file, cb) =>{
-//             const ext = file.originalname;
-//             unique = uuid();
-//             cb(null, `${unique}-${ext}`);
-//             console.log(unique);
-//         }
-//     })
-// });
+// var upload = multer({ storage: storage });
 
-// app.post("/file", function (req, res, next) {
-//   const file = req;
-//   console.log("File name: ",file);
+// app.post("/file", upload.single("file"), function (req, res, next) {
+//   const file = req.file;
 //   if (file) {
 //     res.json(req.file);
 //   } else throw "error";
--- router/fileUpload.js
+++ router/fileUpload.js
@@ -1,74 +1,49 @@
-const aws = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
-const uuid = require('uuid').v4;
-
-const path = require('path');
-const express = require('express');
-
+// const aws = require('aws-sdk');
+// const multer = require('multer');
+// const multerS3 = require('multer-s3');
+// const uuid = require('uuid').v4;
 // const Candidate= require('../models/candidate');
 // const response = require('../utils/Response');
-// const { setTimeout } = require('timers/promises');
+
+// const path = require('path');
+// const express = require('express');
 
 
-// const s3 = new aws.S3({apiVersion: '2006-03-01'});
-
-const s3= new aws.S3({
-    accessKeyId:process.env.AWS_Hemanth_ID,
-    secretAccessKey:process.env.AWS_Hemanth_Key
-});
-
-
-var unique;
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: 'hemanth-fileupload',
-        metadata: (req,file, cb)=>{
-            cb(null,{fieldName: file.fieldname});
-        },
-        key:(req, file, cb) =>{
-            const ext = file.originalname;
-            unique = uuid();
-            cb(null, `${unique}-${ext}`);
-            console.log(unique);
-            exports.uniqueId = unique;
-        }
-        
-    })
-});
-
-
-// const storage = multer.diskStorage({
-//     destination:(req,file,cb) =>{
-//         cb(null,'uploads');
-//     },
-//     filename:(req,file,cb)=>{
-//         const { originalname }=file;
-//         cb(null, `${uuid()}-${originalname}`);
-//     }
+// const s3= new aws.S3({
+//     accessKeyId:process.env.AWSHemanthID,
+//     secretAccessKey:process.env.AWSHemanthKey
 // });
 
-// const upload = multer({storage});
 
-const router = express.Router();
+// var unique;
+// const upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: 'hemanth-upload',
+//         metadata: (req,file, cb)=>{
+//             cb(null,{fieldName: file.fieldname});
+//         },
+//         key:(req, file, cb) =>{
+//             const ext = file.originalname;
+//             unique = uuid();
+//             cb(null, `${unique}-${ext}`);
+//             console.log(unique);
+//         },
+//     })
+// });
 
 
-router.post("/upload",upload.array('upload'),(req,res,next)=>{
-    
-//     setTimeout(()=>{
-        
-//         console.log(candidateId);
-//         const candidateId = Candidate.findById(convertToObjectID(req.body.candidateid));
-//         if(!candidateId){
-//             throw new Error("Candidate details already not found!");
-//         }
-//         const candidate= Candidate.updateOne({resumeId,uniqueId});
-//         response.successReponse({status:201,result:candidate,res});
-// },2000)
-}); 
- 
-module.exports = router;
+
+// const router = express.Router();
+
+
+// router.post("/uploads",upload.array('file'),(req,res,next)=>{
+//     console.log(req.body);
+// });
+
+// module.exports = router;
+
+
 
 
 
